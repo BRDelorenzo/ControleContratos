0dc672460726d60c225c460c669461acb31ea940
BRDelorenzo <64170889+BRDelorenzo@users.noreply.github.com>
2025-06-02 15:10:54 -0300

    Create Contract Registration Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { FormContratoDialogComponent } from './components/form-contrato-dialog/form-contrato-dialog.component';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatCardModule} from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -14,9 +21,17 @@ import {MatCardModule} from '@angular/material/card';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MatDialogModule,
     MatTabsModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule
   ],
   providers: [
     provideClientHydration()
--- src/app/components/form-contrato-dialog/form-contrato-dialog.component.ts
+++ src/app/components/form-contrato-dialog/form-contrato-dialog.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import {MatCardModule} from '@angular/material/card';
-import {MatTabsModule} from '@angular/material/tabs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-contrato-dialog',
@@ -9,16 +8,41 @@ import {MatTabsModule} from '@angular/material/tabs';
   styleUrl: './form-contrato-dialog.component.css',
 })
 export class FormContratoDialogComponent implements OnInit {
+  contratoForm: FormGroup;
+  tiposContrato = [
+    'Prestação de Serviços',
+    'Fornecimento',
+    'Locação',
+    'Consultoria',
+    'Outros'
+  ];
 
-  constructor(public dialogRef: MatDialogRef<FormContratoDialogComponent>){
-
+  constructor(
+    public dialogRef: MatDialogRef<FormContratoDialogComponent>,
+    private fb: FormBuilder
+  ) {
+    this.contratoForm = this.fb.group({
+      numeroContrato: ['', Validators.required],
+      nomeContratado: ['', Validators.required],
+      cnpjContratado: ['', Validators.required],
+      tipoContrato: ['', Validators.required],
+      valorContrato: ['', [Validators.required, Validators.min(0)]],
+      dataInicio: ['', Validators.required],
+      dataFim: ['', Validators.required],
+      descricao: ['', Validators.required]
+    });
   }
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
+
   cancel(): void {
     this.dialogRef.close();
   }
 
+  onSubmit(): void {
+    if (this.contratoForm.valid) {
+      console.log(this.contratoForm.value);
+      this.dialogRef.close(this.contratoForm.value);
+    }
+  }
 }
